da59c49d56bfff87f02a3b2ca4dd36c6b5dffa42
Quin Schurman <schurmanq@gmail.com>
2023-04-24 17:09:14 -0400

    Indents to Spaces

--- server.lua
+++ server.lua
@@ -1,35 +1,35 @@
 RegisterServerEvent('rpx-policejob:server:handcuff', function(target)
     local src = tonumber(source)
-	local char = RPX.GetPlayer(src)
+    local char = RPX.GetPlayer(src)
     if not char then return end
-	if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
-		TriggerClientEvent('rpx-policejob:client:handcuff', target)
-	end
+    if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
+        TriggerClientEvent('rpx-policejob:client:handcuff', target)
+    end
 end)
 
 RegisterServerEvent('rpx-policejob:server:hardcuff', function(target)
     local src = tonumber(source)
-	local char = RPX.GetPlayer(src)
+    local char = RPX.GetPlayer(src)
     if not char then return end
-	if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
-		TriggerClientEvent('rpx-policejob:client:hardcuff', target)
-	end
+    if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
+        TriggerClientEvent('rpx-policejob:client:hardcuff', target)
+    end
 end)
 
 RegisterServerEvent('rpx-policejob:server:hogtie', function(target)
     local src = tonumber(source)
-	local char = RPX.GetPlayer(src)
+    local char = RPX.GetPlayer(src)
     if not char then return end
-	if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
-		TriggerClientEvent('rpx-policejob:client:hogtie', target)
-	end
+    if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
+        TriggerClientEvent('rpx-policejob:client:hogtie', target)
+    end
 end)
 
 RegisterServerEvent('rpx-policejob:server:drag', function(target)
     local src = tonumber(source)
-	local char = RPX.GetPlayer(src)
+    local char = RPX.GetPlayer(src)
     if not char then return end
-	if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
-		TriggerClientEvent('rpx-policejob:client:drag', target, src)
-	end
+    if exports['rpx-core']:HasJobPermission(char.job.name, char.job.rank, "sheriff:general") then
+        TriggerClientEvent('rpx-policejob:client:drag', target, src)
+    end
 end)
